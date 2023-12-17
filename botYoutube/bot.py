118802253051ee2bb2e69226fc2aa6f4351a5cb5
eduardo.murakoshi@groove.tech <eduardo.murakoshi@groove.tech>
2023-12-17 12:04:56 -0300

    bot finalizado

--- botYoutube/bot.py
+++ botYoutube/bot.py
@@ -21,6 +21,7 @@ To fix this, you can either:
 
 Please refer to the documentation for more information at https://documentation.botcity.dev/
 """
+import datetime
 
 # Import for the Desktop Bot
 from botcity.core import DesktopBot
@@ -40,6 +41,14 @@ def main():
 
     print(f"Task ID is: {execution.task_id}")
     print(f"Task Parameters are: {execution.parameters}")
+    
+    # configurando nossos alertas/NO CASO DE INFORMAÇÃO
+    maestro.alert(
+        task_id=execution.task_id,
+        title="BotYoutube - Inicio",
+        message="Estamos iniciando o processo",
+        alert_type=AlertType.INFO
+    )
 
     bot = DesktopBot()
     bot.browse("https://www.youtube.com/watch?v=1bJ4_FDIKy0&list=RDCMUCEmDI96PRniiegjkSmrpZaQ&index=27")
@@ -48,12 +57,16 @@ def main():
     while not bot.find( "unlike", matching=0.97, waiting_time=10000):
         not_found("unlike")
     bot.click()
+    like = 1
     
     while not bot.find( "like", matching=0.97, waiting_time=10000):
         not_found("like")
     bot.click()
+    like = like + 1
 
-    
+    maestro.new_log_entry(activity_label="LikeLogs", 
+                          values={"data_hora": datetime.datetime.now().strftime("%Y-%m-%d %H:%M"),
+                                  "canal": "Youtube", "likes_unlikes": like})
 
     #Uncomment to mark this task as finished on BotMaestro
     maestro.finish_task(
