9ec14735a289aa3a2f9f44269048e70802977259
eduardo.murakoshi@groove.tech <eduardo.murakoshi@groove.tech>
2024-01-21 23:37:41 -0300

    download de arquivo

--- botYoutube/python/pythonProject/CustomerOnboarding/bot.py
+++ botYoutube/python/pythonProject/CustomerOnboarding/bot.py
@@ -1,40 +1,33 @@
 from botcity.web import WebBot, Browser, By
 from webdriver_manager.chrome import ChromeDriverManager
+import requests
 
-def main():
-    bot = WebBot()
-
-    #se o comando abaixo for False, o navegador será aberto, caso
-    #contrário, ficará em segundo plano e não veremos nada
-    bot.headless = False
-    bot.browser = Browser.CHROME
-
-    bot.driver_path = ChromeDriverManager().install()
-
-    bot.browse("https://www.botcity.dev")
+#link do botão de download do arquivo, da página:
+#https://developer.automationanywhere.com/challenges/automationanywherelabs-customeronboarding.html
+url = 'https://aai-devportal-media.s3.us-west-2.amazonaws.com/challenges/customer-onboarding-challenge.csv'
+r = requests.get(url, allow_redirects=True)
+open('customer-onboarding-challenge.csv', 'wb').write(r.content)
 
 
-    # Wait 3 seconds before closing
-    bot.wait(3000)
-
-    bot.maximize_window()
-    bot.wait(3000)
-    # Finish and clean up the Web Browser
-    # You MUST invoke the stop_browser to avoid
-    # leaving instances of the webdriver open
-    bot.stop_browser()
-
-    # Uncomment to mark this task as finished on BotMaestro
-    # maestro.finish_task(
-    #     task_id=execution.task_id,
-    #     status=AutomationTaskFinishStatus.SUCCESS,
-    #     message="Task Finished OK."
-    # )
+# bot = WebBot()
+#se o comando abaixo for False, o navegador será aberto, caso
+#contrário, ficará em segundo plano e não veremos nada
+# bot.headless = False
+# bot.browser = Browser.CHROME
+# bot.driver_path = ChromeDriverManager().install()
+# bot.browse("https://www.botcity.dev")
+# Wait 3 seconds before closing
+# bot.wait(3000)
+# bot.maximize_window()
+# bot.wait(3000)
+# Finish and clean up the Web Browser
+# You MUST invoke the stop_browser to avoid
+# leaving instances of the webdriver open
+# bot.stop_browser()
 
 
 def not_found(label):
     print(f"Element not found: {label}")
 
 
-if __name__ == '__main__':
-    main()
+
